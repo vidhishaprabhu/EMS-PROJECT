8c5ffaf43b828ab7c267b81c2bfc88dc2248aabb
vidhishaprabhu <vidhishaprabhu@gmail.com>
2026-03-17 14:06:04 +0530

    Done with sidebar design and created component for admin dashboard and also service for admin dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'admin/dashboard',
+    component:DashboardComponent
+  }
+];
