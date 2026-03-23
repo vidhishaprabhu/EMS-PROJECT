addac54488dbd23d278874ea556f590907b3e460
vidhishaprabhu <vidhishaprabhu@gmail.com>
2026-03-23 13:13:02 +0530

    Donw with design part of change password form

--- controllers/admin.js
+++ controllers/admin.js
@@ -22,3 +22,44 @@ exports.createAdmin = async (req, res) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 };
+exports.changePasswordAdmin = async (req, res) => {
+  try {
+    const { currentPassword, newPassword, confirmPassword } = req.body;
+    if (!currentPassword || !newPassword || !confirmPassword) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    if (newPassword !== confirmPassword) {
+      return res
+        .status(400)
+        .json({ message: "New password and confirm password do not match" });
+    }
+
+    if (newPassword.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "New password must be at least 6 characters" });
+    }
+
+    const adminId = req.user.id;
+
+    const admin = await Admin.findById(adminId);
+    if (!admin) {
+      return res
+        .status(404)
+        .json({ message: `Admin with ${adminId} not found` }); 
+    }
+
+    const isMatch = await bcrypt.compare(currentPassword, admin.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Current password is incorrect" }); 400
+    }
+
+    admin.password = await bcrypt.hash(newPassword, 10);
+    await admin.save();
+
+    return res.status(200).json({ message: "Password changed successfully",admin:admin });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const router=express();
-const {createAdmin}=require('../controllers/admin')
-
-router.post('/',createAdmin)
-
+const {createAdmin,changePasswordAdmin}=require('../controllers/admin')
+const {verifyToken}=require('../middleware/authMiddleware')
+router.post('/',verifyToken,createAdmin)
+router.put('/change-password-admin',verifyToken,changePasswordAdmin)
 module.exports=router
