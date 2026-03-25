b699e9ea98e630387ca8819b1d08842d2a665b2e
vidhishaprabhu <vidhishaprabhu@gmail.com>
2026-03-25 11:43:33 +0530

    Added the automatic calculation of total in salary form

--- src/app/components/admin/salaries/salaries.component.ts
+++ src/app/components/admin/salaries/salaries.component.ts
@@ -24,6 +24,17 @@ export class SalariesComponent {
   ngOnInit(){
     this.getAllDept()
     this.getAllEmp()
+    this.salaryForm.get('basic')?.valueChanges.subscribe(()=>this.calaculateTotal())
+    this.salaryForm.get('bonus')?.valueChanges.subscribe(()=>this.calaculateTotal())
+    this.salaryForm.get('deduction')?.valueChanges.subscribe(()=>this.calaculateTotal())
+  }
+  calaculateTotal(){
+    const basic:any=this.salaryForm.get('basic')?.value || 0
+    const bonus=this.salaryForm.get('bonus')?.value || 0
+    const deduction:any=this.salaryForm.get('deduction')?.value || 0
+
+    const total:any=basic + bonus - deduction
+    this.salaryForm.get('total')?.setValue(total,{emitValue:false});
   }
   getAllEmp(){
     this.adminService.getAllEmployee().subscribe((res: any) => {
